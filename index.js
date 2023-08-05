4e1f4b2ab343ee8c9cfc4bda9b444e3e0b0dc60d
Dricc-new <diegoramoncc17@gmail.com>
2023-08-05 17:21:35 -0400

    using cors package

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import express from "express"
+import cors from cors
 
 const app = express()
 
-app.use('/ping', (req, res) =>{
+// Using cors module
+app.use(cors({
+    origin:['http://localhost:5173','http://localhost:8000'], // * significaria que todos acceden
+    // To restrict the methods use the methods property
+}))
+
+/* // If you don't want to use cors library use this code
+app.use((req, res, next) => {
+    // This defines from which origin or origins can request requests
+    res.append('Access-Control-Allow-Origin', ['http://localhost:5173'])
+    
+    // This defines which method they can request
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    
+    // This defines what can be requested
+    res.append('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+}) */
+
+
+app.use('/ping', (req, res) => {
     res.send('You have connected with the API')
 })
 
